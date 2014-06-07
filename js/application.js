325cd6ea3b7ed6a4236191c5dab83b20ba46c060
Eric Tan <20925931@student.uwa.edu.au>
2014-06-07 17:37:42 +0800

    Basic slider added

--- js/application.js
+++ js/application.js
@@ -1,3 +1,23 @@
-$(function () {
-
+$(document).ready(function(){
+	$('#slideshow').bjqs({
+		//size of slider
+		width: 960,
+		height: 350,
+		
+		//control and marker configuration
+		showcontrols: true,
+		centercontrols: true,
+		nexttext: '>',
+		prevtext: '<',
+		showmarkers: false,
+		
+		//interaction values
+		keyboardnav: true,
+		hoverpause: true,
+		
+		//presentational options
+		usecaptions: false,
+		randomstart: false,
+		responsive: true
+	});
 });
